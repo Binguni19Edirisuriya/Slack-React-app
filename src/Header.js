f75763a3401b980ee7c6e5c670051756e9bf28c9
Binguni19Edirisuriya <biguniemail@gmail.com>
2021-02-07 00:51:08 +0530

    Basic Header with Search

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./Header.css";
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon  from '@material-ui/icons/HelpOutline';
+
+
+
+function Header() {
+    return (
+        <div className = "header">
+        <div className = 'header_left'>
+        <Avatar className = "header_avatar"
+            alt= "AAA"
+            src= ""
+        />
+
+        <AccessTimeIcon />
+
+        </div>
+        <div className = 'header_search'>
+        <SearchIcon />
+        <input placeholder = "Search by Name"/>
+
+        </div>
+
+        <div className = 'header_right'>
+        <HelpOutlineIcon />
+
+        </div>
+        
+            
+        </div>
+    )
+}
+
+export default Header;
