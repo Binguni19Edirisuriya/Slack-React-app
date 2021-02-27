eacc843c782bd71afaae8ef4cf8c821a8cc7e63a
Binguni19Edirisuriya <biguniemail@gmail.com>
2021-02-28 01:58:37 +0530

    switching between channels without refefreshing and implement chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import './Chat.css';
 import {useParams} from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
+import Message from './Message';
 
 function Chat() {
     const { roomId } = useParams();
+    const [roomDetails, setRoomDetails] = useState(null)
+    const [roomMessages, setRoomMessages] = useState([])
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms').doc(roomId).onSnapshot((snapshot) => (
+                setRoomDetails(snapshot.data())
+            ))
+        
+        }
+        db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp' , 'asc').onSnapshot((snapshot) =>
+            setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }, [roomId]);
+
+    console.log(roomDetails);
+    console.log("msg" , roomMessages)
+
+
     return (
         <div className= "chat">
-        <h2> {roomId} room</h2>
+        
             <div className= "chat_header">
-            <div className= "">
+            <div className= "chat_headerLeft">
                 <h4 className= "channel_name">
-                    <strong>#general</strong>
+                    <strong>#{roomDetails?.name}</strong>
                     <StarBorderOutlinedIcon/>
                 </h4>
             </div>
@@ -20,9 +41,21 @@ function Chat() {
             <div className = "chat_headerRight">
                 <p>
                     <InfoOutlinedIcon />
+                    Details
                 </p>
             </div>
 
+            </div>
+
+            <div className= "chat_messages">
+               {roomMessages.map(({message , timestamp , user, userImage}) => (
+                   <Message
+                       message={message}
+                       timestamp={timestamp}
+                       user={user}
+                       userImage={userImage}
+                   />
+               ))} 
             </div>
         </div>
     )
--- src/SideBarOption.js
+++ src/SideBarOption.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import db from './firebase';
 import "./SideBarOption.css";
 
-function SideBarOption({Icon, title}) {
+function SideBarOption({Icon, title , id, addChannelOption}) {
+
+    const history = useHistory();
+
+    const selectChannel = () => {
+        if(id){
+            history.push(`/room/${id}`)
+        } else{
+            history.push(title);
+        }
+    };
+
+    const addChannel = () => {
+        const channelName = prompt('Please enter the Channel name');
+
+        if (channelName) {
+            db.collection("rooms").add({
+                name: channelName,
+            })
+        }
+    };
+
+
+
     return (
-        <div className= "sidebarOption">
+        <div className= "sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
             {Icon && <Icon className = "sidebarOption_icon"/>}
             {Icon ? (
                 <h3>{title}</h3>
